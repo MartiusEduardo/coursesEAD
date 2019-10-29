78857cc2d310f372ef519a5af7073545ee7b7430
Martius Eduardo <35742758+MartiusEduardo@users.noreply.github.com>
2019-10-29 12:17:33 -0300

    Add files via upload

---
+++ Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,6 @@
+namespace Simulacao.Services {
+    public class AuthMessageSenderOptions {
+        public string SendGridUser = "MartiusEduardo";
+        public string SendGridKey = "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";
+    }
+}
